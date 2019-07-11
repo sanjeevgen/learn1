ee7ece2fb3341cada250efabe9d1a6e5c4d99c5d
sanjeevgen <52686003+sanjeevgen@users.noreply.github.com>
2019-07-10 23:38:39 -0500

    First Commit

---
+++ Proj1/src/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sanjeev
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
